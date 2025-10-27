24591a0a9c603429cdf7f0639afa99c73a5546a1
Saad Hazim <saadnamik@gmail.com>
2025-10-26 19:04:39 -0700

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('year').textContent = new Date().getFullYear();
-document.querySelector('.nav__toggle')?.addEventListener('click', () => {
-  const list = document.querySelector('.nav__list');
-  list.style.display = (list.style.display === 'flex' ? 'none' : 'flex');
-});
